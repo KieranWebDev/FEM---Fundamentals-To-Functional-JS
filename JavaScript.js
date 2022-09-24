bff4fc03e84a04b32ae5bd1ddb637023a9719149
Kieran <kieran.webdevelopment@gmail.com>
2022-09-24 10:52:09 +0100

    destructuring excercise

--- JavaScript.js
+++ JavaScript.js
@@ -1,23 +1,37 @@
-var box = [];
-box['material'] = 'cardboard';
-box[0] = 'meow';
-box['%$'] = 'testing 123';
-box.push('hello');
+// var box = [];
+// box['material'] = 'cardboard';
+// box[0] = 'meow';
+// box['%$'] = 'testing 123';
+// box.push('hello');
 
-console.log(box);
+// console.log(box);
 
-let game = {};
+// let game = {};
 
-game.murderer = '??';
+// game.murderer = '??';
 
-game.weapons = [
-  { type: 'gun', location: 'lab' },
-  { type: 'hammer', location: 'garden' },
-];
+// game.weapons = [
+//   { type: 'gun', location: 'lab' },
+//   { type: 'hammer', location: 'garden' },
+// ];
 
-game.characters = [];
+// game.characters = [];
 
-game.characters[0] = 'Miss Scarlet';
+// game.characters[0] = 'Miss Scarlet';
 
-game.characters.push('Mr.jim');
-console.log(game);
+// game.characters.push('Mr.jim');
+// console.log(game);
+
+// const { name, weapon, rusty } = {
+//   name: 'Rusty',
+//   room: 'kitchen',
+//   weapon: 'candlestick',
+// };
+
+const { name, weapon, room } = {
+  name: 'Rusty',
+  room: 'kitchen',
+  weapon: 'candlestick',
+};
+console.log(name);
+console.log(room);
